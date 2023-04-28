06988c8061eea4b44909fec77c8fdb2bc413dd29
Vukipoo <perovicv515@gmail.com>
2023-04-27 20:51:49 -0700

    added enough functions for basic math calculations

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+let a = null;
+let b = null;
+let opp = null;
+
+
+const display = document.querySelector('.display')
+const numButtons = document.querySelectorAll('.buttons .num')
+const symbolButtons = document.querySelectorAll('.buttons .symbol')
+const ac = document.querySelector('.ac')
+ac.addEventListener('click', () => {
+    display.textContent = null
+})
+//a for each loop that loops through all the buttons
+//assining each one a click event listener that displays
+//its assigned html
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (opp === null) {
+            if (a === null) {
+                a = parseFloat(button.textContent);
+                display.textContent = a;
+            } else {
+                a = parseFloat(a.toString() + button.textContent);
+                display.textContent = a;
+            }
+        } else {
+            if (b === null) {
+                b = parseFloat(button.textContent);
+                display.textContent = b;
+            } else {
+                b = parseFloat(b.toString() + button.textContent);
+                display.textContent = b;
+            }
+        }
+    });
+});
+
+symbolButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        display.textContent += button.textContent
+        opp = button.textContent
+    })
+})
+//equals function
+const equals = document.querySelector('.equals')
+equals.addEventListener('click', () => {
+    const result = operate(a, b, opp)
+    display.textContent = result
+})
+
+//4 Basic math operators 
 function add (a, b) {
     return a + b
 }
@@ -9,5 +60,21 @@ function multiply (a, b) {
 }
 function divide (a, b) {
     return a / b
+}
+
+//Operator function
+function operate(a, b, opp){
+    switch(opp){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '/':
+            return divide(a, b);
+        case '*':
+            return multiply(a,b)
+    
+       
+    }
 }
 
